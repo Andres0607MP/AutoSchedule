875213108f5222e2b239d2f9f5592dbce13f7f17
Andres0607MP <andresmancera06156@gmail.com>
2025-12-07 12:55:39 -0500

    Configurar manejador global de excepciones DRF

--- config/settings/base.py
+++ config/settings/base.py
@@ -114,6 +114,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'EXCEPTION_HANDLER': 'config.api.api_exceptions.custom_exception_handler',
 }
 
 
