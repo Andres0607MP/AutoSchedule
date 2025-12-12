5f2ee84467a127f531a7ba6dfb4b27f001d4572e
Andres0607MP <andresmancera06156@gmail.com>
2025-12-12 10:10:43 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,24 @@ else
 fi
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --settings=config.settings.prod
+python manage.py collectstatic --noinput --settings=config.settings.prod <<'PY'
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('SUPERUSER_USERNAME')
+email = os.environ.get('SUPERUSER_EMAIL', '')
+password = os.environ.get('SUPERUSER_PASSWORD')
+if username and password:
+	if not User.objects.filter(username=username).exists():
+		User.objects.create_superuser(username=username, email=email, password=password)
+		print('Superuser created:', username)
+	else:
+		print('Superuser already exists:', username)
+else:
+	print('SUPERUSER_USERNAME or SUPERUSER_PASSWORD not set; skipping superuser creation')
+PY
+fi
+
 
 echo "Starting gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
