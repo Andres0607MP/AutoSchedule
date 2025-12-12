b8a1f6fd330250e057b8a36a86376d3142b09b7e
DanieLuque <Danielcaluque502@gmail.com>
2025-12-11 22:31:44 -0500

    ajusto fecha y hora

--- apps/queue_app/serializers.py
+++ apps/queue_app/serializers.py
@@ -1,7 +1,26 @@
 from rest_framework import serializers
 from .models import QueueTicket
+from datetime import datetime, date
 
 class QueueTicketSerializer(serializers.ModelSerializer):
+    hora_estimada = serializers.TimeField(required=False)
+
     class Meta:
         model = QueueTicket
         fields = '__all__'
+
+    def validate(self, data):
+        """
+        Si solo envían la hora, completar la fecha automáticamente (hoy).
+        Si envían fecha+hora completa también funciona.
+        """
+        hora = data.get("hora_estimada")
+
+        if isinstance(hora, datetime):
+            return data
+
+        if hora:
+            hoy = date.today()
+            data["hora_estimada"] = datetime.combine(hoy, hora)
+
+        return data
